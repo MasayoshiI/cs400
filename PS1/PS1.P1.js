bf74df6c5bf9741a015a5058051d06305c4d66ba
Masayoshi Iwasa <masayoshiiwasa@dhcp-wifi-8021x-155-41-37-174.bu.edu>
2020-02-05 17:15:49 -0500

    p1 added

---
+++ PS1/PS1.P1.js
@@ -0,0 +1,15 @@
+// Author: Masayoshi Iwasa
+// File: PS1P1 
+
+// Write a function that takes a string as its input and returns a new string that contains all of the
+// letters in the original string, but in alphabetical order. Ignore punctuation and numbers. Test
+// your function using the string ‘supercalifragilisticexpialidocious’.
+
+const letters = word => 
+    { 
+        // 
+        let chars = word.split('').sort();
+    chars = chars.filter((a, b) => chars.indexOf(a) === b).join()
+    return chars
+}
+console.log(`All letters in string is: ${letters('supercalifragilisticexpialidocious')}`) 
