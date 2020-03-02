efb427504d6a586e057e05af061a740bfd4abeed
Masayoshi Iwasa <masayoshiiwasa@dhcp-wifi-8021x-155-41-103-141.bu.edu>
2020-03-02 00:21:17 -0500

    PS3

---
+++ PS3/myapp/routes/ps3.js
@@ -0,0 +1,18 @@
+
+var express = require('express');
+var router = express.Router();
+
+/* GET string. */
+router.get('/', function(req, res, next) {
+  res.render('get', {string: 'PS3 Completed'});
+});
+
+/* POST string. */
+router.post('/', function(req, res, next) {
+  const str = req.body.string;
+  const len = str.length;
+  res.render('post', {string: str, len: len});
+});
+
+
+module.exports = router;
