42d02dfe96bcb385a2bcf443b12aeefb5ee7d834
Masayoshi Iwasa <miwasa20@bu.edu>
2020-04-29 22:32:54 +0900

    PS6

---
+++ PS6/src/app/mock.data.ts
@@ -0,0 +1,29 @@
+export let data = 
+    {
+        coord: { lon: 139.69, lat: 35.69 },
+        weather: [ { id: 800, main: 'Clear', description: 'clear sky', icon: '01n' } ],
+        base: 'stations',
+        main: {
+          temp: 290.35,
+          feels_like: 288.16,
+          temp_min: 289.82,
+          temp_max: 290.93,
+          pressure: 1019,
+          humidity: 67
+        },
+        visibility: 10000,
+        wind: { speed: 3.6, deg: 190 },
+        clouds: { all: 0 },
+        dt: 1588165648,
+        sys: {
+          type: 1,
+          id: 8074,
+          country: 'JP',
+          sunrise: 1588103483,
+          sunset: 1588152334
+        },
+        timezone: 32400,
+        id: 1850144,
+        name: 'Tokyo',
+        cod: 200
+      }
